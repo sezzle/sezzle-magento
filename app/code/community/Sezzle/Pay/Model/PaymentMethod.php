8a9d7f3a6189519bb07ed77fad57eb7960b4eb81
Rishi Mukherjee <rishi.mukherjee@sezzle.com>
2017-09-25 20:54:26 +0530

    Added model

---
+++ app/code/community/Sezzle/Pay/Model/PaymentMethod.php
@@ -0,0 +1,33 @@
+<?php
+class Sezzle_Pay_Model_PaymentMethod extends Mage_Payment_Model_Method_Abstract{
+    /**
+     * Availability options
+     */ 
+    private $LOG_FILE_NAME = 'sezzle-pay.log';
+    protected $_code = 'pay';
+    protected $_isGateway               = true;
+    protected $_canAuthorize            = true;
+    protected $_canCapture              = true;
+    protected $_canCapturePartial       = true;
+    protected $_canRefund               = false;
+    protected $_canVoid                 = false;
+    protected $_canUseInternal          = true;
+    protected $_canUseCheckout          = true;
+    protected $_canUseForMultishipping  = true;
+    protected $_canSaveCc               = false;
+    protected $_isInitializeNeeded      = false;
+    /**
+    * @return Mage_Checkout_Model_Session
+    */
+    protected function _getCheckout()
+    {
+       return Mage::getSingleton('checkout/session');
+    }
+    // Construct the redirect URL
+    public function getOrderPlaceRedirectUrl()
+    {   
+        $redirect_url = Mage::getUrl('pay/payment/redirect');
+        Mage::Log("Step 2 Process: Getting the redirect URL: $redirect_url", Zend_Log::DEBUG, $this->LOG_FILE_NAME);
+        return $redirect_url;      
+    }
+}
