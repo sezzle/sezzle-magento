964f8c47a78d99b76e2cbd09c512c71300134d9a
Rishi Mukherjee <rishi.mukherjee@sezzle.com>
2017-09-26 13:12:54 +0530

    Prepared data for sending to gateway. Created signature using sha256 for verification.

--- app/code/community/Sezzle/Pay/Model/PaymentMethod.php
+++ app/code/community/Sezzle/Pay/Model/PaymentMethod.php
@@ -30,4 +30,18 @@ class Sezzle_Pay_Model_PaymentMethod extends Mage_Payment_Model_Method_Abstract{
         Mage::Log("Step 2 Process: Getting the redirect URL: $redirect_url", Zend_Log::DEBUG, $this->LOG_FILE_NAME);
         return $redirect_url;      
     }
+
+    public function authorize(Varien_Object $payment, $amount){
+        Mage::Log('Step 0 Process: Authorize', Zend_Log::DEBUG, $this->LOG_FILE_NAME);
+        return $this;
+    }
+    /**
+     * this method is called if we are authorising AND
+     * capturing a transaction
+     */
+    public function capture(Varien_Object $payment, $amount)
+    {
+        Mage::Log('Step 1 Process: Create and capture the process', Zend_Log::DEBUG, $this->LOG_FILE_NAME);
+        return $this;
+    }
 }
--- app/code/community/Sezzle/Pay/controllers/PaymentController.php
+++ app/code/community/Sezzle/Pay/controllers/PaymentController.php
@@ -23,7 +23,7 @@ class Sezzle_Pay_PaymentController extends Mage_Core_Controller_Front_Action
             
             // Set status to payment pending
             $order->setState(Mage_Sales_Model_Order::STATE_PENDING_PAYMENT, true)->save();
-            $accountID = Mage::getStoreConfig('payment/imojo/public_key', $storeId);
+            $accountID = Mage::getStoreConfig('payment/pay/public_key', $storeId);
 
             // Cost details
             $amount = $order-> getBaseGrandTotal();
@@ -37,6 +37,7 @@ class Sezzle_Pay_PaymentController extends Mage_Core_Controller_Front_Action
             $billingPhone = $billingAddress->getTelephone();
             $billingZip = $billingAddress->getPostcode();
             $billingState = $billingAddress->getRegion();
+            $billingCountry = $billingAddress->getCountry();
 
             // User details
             $email = $order->getCustomerEmail();
@@ -51,12 +52,74 @@ class Sezzle_Pay_PaymentController extends Mage_Core_Controller_Front_Action
             $shippingCity = $shippingAddress->getCity();
             $shippingPhone = $shippingAddress->getTelephone();
             $shippingZip = $shippingAddress->getPostcode();
-            $shippingState = $shippingAddress->getRegion();
+            $shippingState = $shippingAddress->getRegionCode();
+            $shippingCountry = $shippingAddress->getCountry();
+            $shippingFirstname = $shippingAddress->getFirstname();
+            $shippingLastname = $shippingAddress->getLastname();
             
             // Reference
             $reference = $orderId;
             $countryCode = Mage::getStoreConfig('general/country/default');
             $shopName = Mage::app()->getStore()->getName();
+            $testMode = false;
+
+            $data = Array(
+                "x_account_id" => $accountID,
+                "x_amount" => $amount,
+                "x_currency" => $currency,
+                "x_customer_billing_address1" => $billingAddress1,
+                "x_customer_billing_address2" => $billingAddress2,
+                "x_customer_billing_city" => $billingCity,
+                "x_customer_billing_country" => $billingCountry,
+                "x_customer_billing_phone" => $billingPhone,
+                "x_customer_billing_zip" => $billingZip,
+                "x_customer_billing_state" => $billingState,
+                "x_customer_email" => $email,
+                "x_customer_first_name" => $firstName,
+                "x_customer_last_name" => $lastName,
+                "x_customer_phone" => $phone,
+                "x_customer_shipping_address1" => $shippingAddress1,
+                "x_customer_shipping_address2" => $shippingAddress2,
+                "x_customer_shipping_city" => $shippingCity,
+                "x_customer_shipping_country" => $shippingCountry,
+                "x_customer_shipping_first_name" => $shippingFirstname,
+                "x_customer_shipping_last_name" => $shippingLastname,
+                "x_customer_shipping_phone" => $shippingPhone,
+                "x_customer_shipping_zip" => $shippingZip,
+                "x_customer_shipping_state" => $shippingState,
+                "x_reference" => $orderId,
+                "x_test" => $testMode,
+                'x_url_complete' => Mage::getUrl('pay/payment/success', array('transaction_id' => $order_id)),
+                'x_url_cancel' => Mage::getUrl('pay/payment/cancel', array('transaction_id' => $order_id)),
+            );
+
+            // Create the signature
+            $ver = explode('.', phpversion());
+            $major = (int) $ver[0];
+            $minor = (int) $ver[1];
+            if($major >= 5 and $minor >= 4){
+                ksort($data, SORT_STRING | SORT_FLAG_CASE);
+            }
+            else{
+                uksort($data, 'strcasecmp');
+            }
+            $message = "";
+            foreach ($data as $key => $value) {
+                $message .= "$key$value";
+            }
+            $sign = hash_hmac("sha256", $message, $private_salt);
+            $data["x_signature"] = $sign;
+
+            Mage::log(
+                "orderId=$orderId, accountID=$accountID, amount=$amount, currency=$currency,
+                billingAddress1=$billingAddress1, billingAddress2=$billingAddress2, billingCity=$billingCity,
+                billingPhone=$billingPhone, billingZip=$billingZip, billingState=$billingState, email=$email, firstName=$firstName,
+                phone=$phone, shippingAddress1=$shippingAddress1, shippingAddress2=$shippingAddress2, shippingCity=$shippingCity,
+                shippingPhone=$shippingPhone, shippingZip=$shippingZip, shippingState=$shippingState, reference=$reference,
+                countryCode=$countryCode, shopName=$shopName", Zend_Log::DEBUG,
+                $this->LOG_FILE_NAME
+            );
+
         } catch (Exception $e){
             Mage::logException($e);
             Mage::log($e, Zend_Log::ERR, $this->LOG_FILE_NAME);
