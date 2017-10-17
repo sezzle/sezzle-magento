d3e1c8e193930b6a77b41242a9d70902de114050
Rishi Mukherjee <rishi.mukherjee@sezzle.com>
2017-10-17 11:21:01 +0530

    Updated readme

--- app/code/community/Sezzle/Pay/Block/ProductConfig.php
+++ app/code/community/Sezzle/Pay/Block/ProductConfig.php
@@ -4,14 +4,15 @@ class Sezzle_Pay_Block_ProductConfig extends Mage_Core_Block_Template
 {
     public function getJsConfig() 
     {
-        $targetXPath = Mage::getStoreConfig('sezzle_pay/product_widget/xpath');
-        $renderToPath = Mage::getStoreConfig('sezzle_pay/product_widget/renderXPath');
+        $targetXPath = explode('|', Mage::getStoreConfig('sezzle_pay/product_widget/xpath'));
+        $renderToPath = explode('|', Mage::getStoreConfig('sezzle_pay/product_widget/renderXPath'));
         $forcedShow = Mage::getStoreConfig('sezzle_pay/product_widget/forced_show') == "1" ? true : false;
         $alignment = Mage::getStoreConfig('sezzle_pay/product_widget/alignment');
         $merchantID = Mage::getStoreConfig('sezzle_pay/product_widget/merchant_id');
         $theme = Mage::getStoreConfig('sezzle_pay/product_widget/theme');
         $widthType = Mage::getStoreConfig('sezzle_pay/product_widget/width-type');
         $imageUrl = Mage::getStoreConfig('sezzle_pay/product_widget/image-url');
+        $hideClasses = explode('|', Mage::getStoreConfig('sezzle_pay/product_widget/hide-classes'));
 
         return array(
             'targetXPath'          => $targetXPath,
@@ -24,7 +25,8 @@ class Sezzle_Pay_Block_ProductConfig extends Mage_Core_Block_Template
             'widgetType'           => 'product-page',
             'minPrice'             => 0,
             'maxPrice'             => 100000,
-            'imageUrl'             => $imageUrl
+            'imageUrl'             => $imageUrl,
+            'hideClasses'          => $hideClasses,
         );
     }
 }
