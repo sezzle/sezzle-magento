a48b08c552442c919ce7d93ea56bf27c55d17830
Rishi Mukherjee <rishi.mukherjee@sezzle.com>
2017-11-02 19:28:29 +0530

    Added API router class to make request to gateway

---
+++ app/code/community/Sezzle/Pay/Model/Api/Router.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * Used for creating routes based on selected config
+ */
+class Sezzle_Pay_Model_Api_Router {
+
+    const API_MODE_STAGING = 'staging';
+    const API_MODE_LOCAL   = 'local';
+    const API_MODE_LIVE = 'live';
+    const API_MODE_SANDBOX = 'sandbox';
+
+    // Returns the authentication token url
+    protected function getAuthTokenUrl() {
+        return getBaseApiUrl() . '/authentication';
+    }
+
+    // Returns base api url
+    protected function getBaseApiUrl() {
+        $apiMode      = Mage::getStoreConfig('payment/pay/' . Sezzle_Pay_Model_PaymentMethod::API_MODE_CONFIG_FIELD);
+        $overrideBaseUrl = Mage::getStoreConfig('payment/pay/' . Sezzle_Pay_Model_PaymentMethod::API_BASE_URL_CONFIG_FIELD);
+        if ($overrideBaseUrl) {
+            return $this->removeSlashFromUrl($overrideBaseUrl);
+        }
+        switch ($apiMode) {
+            case self::API_MODE_STAGING:
+            return 'https://staging.api.sezzle.com/v1';
+            case self::API_MODE_SANDBOX:
+            return 'https://sandbox.api.sezzle.com/v1';
+            case self:API_MODE_LOCAL:
+            return 'http://127.0.0.1:9002/v1';
+            case self:API_MODE_LIVE:
+            return 'https://api.sezzle.com/v1';
+            default:
+            return 'https://api.sezzle.com/v1';
+        }
+    }
+
+    // Removes / from end of url if present
+    protected function removeSlashFromUrl($url) {
+        if(substr($url, -1) == '/') {
+            $url = substr($string, 0, -1);
+        }
+        return $url;
+    }
+}
--- app/code/community/Sezzle/Pay/Model/PaymentMethod.php
+++ app/code/community/Sezzle/Pay/Model/PaymentMethod.php
@@ -20,6 +20,13 @@ class Sezzle_Pay_Model_PaymentMethod extends Mage_Payment_Model_Method_Abstract
     protected $_formBlockType           = 'sezzle_pay/form_paylater';
 
     /**
+     * Constants
+     */
+    const API_PUBLIC_KEY_CONFIG_PATH = 'payment/pay/public_key';
+    const API_PRIVATE_KEY_CONFIG_PATH = 'payment/pay/private_key';
+    const API_MODE_CONFIG_FIELD = 'api_mode';
+
+    /**
     * @return Mage_Checkout_Model_Session
     */
     protected function _getCheckout()
@@ -33,4 +40,54 @@ class Sezzle_Pay_Model_PaymentMethod extends Mage_Payment_Model_Method_Abstract
         Mage::Log("Step 2 Process: Getting the redirect URL: $redirectUrl", Zend_Log::DEBUG, $this->_logFileName);
         return $redirectUrl;
     }
+
+    // Get the auth data to be sent to sezzle API to get a token
+    protected function getSezzleAuthHeader() {
+        $merchantPublicKey     = trim($this->getConfigData(self::API_PUBLIC_KEY_CONFIG_PATH));
+        $merchantPrivateKey    = trim($this->getConfigData(self::API_PRIVATE_KEY_CONFIG_PATH));
+
+        return array(
+            "public_key" => $merchantPublicKey,
+            "private_key" => $merchantPrivateKey
+        );
+    }
+
+    // Get auth token from Sezzle API
+    protected function getSezzleAuthToken() {
+        $response = $this->_sendApiRequest(
+            $this->getApiRouter()->getAuthTokenUrl(),
+            $this->getSezzleAuthHeader()
+        );
+        if ($result->isError()) {
+            throw Mage::exception(
+                'Sezzle_Pay',
+                __('Sezzle Pay API Error: %s', $result->getMessage())
+            );
+        }
+        $resultObject = json_decode($result->getBody(), true);
+        $token = $resultObject['token'];
+        if (empty($orderToken)) {
+            throw Mage::exception(
+                'Sezzle_Pay',
+                'Sezzle Pay API Error: Cannot get auth token.'
+            );
+        }
+        return $token;
+    }
+
+    // Get auth token from Sezzle API
+    protected function getApiRouter() {
+        return Mage::getModel('pay/api_router');
+    }
+
+    // send request
+    protected function _sendApiRequest($url, $body) {
+        $client = new Varien_Http_Client($url);
+        if ($body !== false) {
+            $client->setRawData($coreHelper->jsonEncode($body), 'application/json');
+        }
+        $response = $client->request();
+        return $response;
+    }
+
 }
