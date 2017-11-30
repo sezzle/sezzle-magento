dcc8c321f341e1fb71590dd72936b450a094e4ed
Rishi Mukherjee <rishi.mukherjee@sezzle.com>
2017-11-30 13:49:14 +0530

    Get order data of last 24 hours.

--- app/code/community/Sezzle/Pay/Model/Observer.php
+++ app/code/community/Sezzle/Pay/Model/Observer.php
@@ -2,7 +2,32 @@
 class Sezzle_Pay_Model_Observer
 {
     public function sendDailyData(Mage_Cron_Model_Schedule $schedule) {
-        $this->helper()->log("I ran");
+        $today = date("Y-m-d H:i:s");
+        $yesterday = date("Y-m-d H:i:s", strtotime("-1 days"));
+
+        $yesterday = date('Y-m-d H:i:s', strtotime($yesterday));
+        $today = date('Y-m-d H:i:s', strtotime($today));
+
+        $orders = Mage::getModel('sales/order')->getCollection()
+            ->addFieldToFilter('status',
+                array(
+                    'eq' => 'complete',
+                    'eq' => 'processing'
+                )
+            )
+            ->addAttributeToFilter('created_at',
+                array(
+                    'from' => $yesterday,
+                    'to' => $today
+                )
+            )
+            ->addAttributeToSelect('customer_email')
+            ->addAttributeToSelect('status');
+        foreach ($orders as $order) {
+            $email = $order->getCustomerEmail();
+            $status = $order->getStatus();
+            $this->helper()->log("$email $status");
+        }
     }
 
     protected function helper()
