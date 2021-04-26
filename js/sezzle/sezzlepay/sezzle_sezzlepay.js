4ee5cad555baf9ca80b30e2215c92a78a32a571d
sitikantha mallik <sitikantha.mallik@sezzle.coom>
2021-04-26 18:07:40 +0530

    WID-479-default-configs-magento1 first commit

--- js/sezzle/sezzlepay/sezzle_sezzlepay.js
+++ js/sezzle/sezzlepay/sezzle_sezzlepay.js
@@ -10,5 +10,28 @@
     script.type = 'text/javascript';
     script.src = 'https://widget.sezzle.com/v1/javascript/price-widget?uuid=' + merchant_id;
     document.head.appendChild(script);
+    document.sezzleConfig = {
+      "configGroups": [
+              {
+                      "targetXPath": ".product-info-main/.price-wrapper/.price",
+                      "renderToPath": "../../../..",
+                      "relatedElementActions": [
+                              {
+                                      "relatedPath": ".",
+                                      "initialAction": function(r,w){
+                                              if(getComputedStyle(r).textDecoration.indexOf("line-through") > -1){
+                                                      w.style.display = "none"
+                                              }
+                                      }
+                              }
+                      ]
+              },
+              {
+                      "targetXPath": ".amount/STRONG-0/.price",
+                      "renderToPath": "../../../../..",
+                      "urlMatch": "cart"
+              }
+      ]
+}
   }
 })();
